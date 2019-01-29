12658c62b89d6d5eb90540ae7ac17318dec2614d
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-29 15:47:53 -0500

    Create Salary_linear_regression.py

---
+++ Salary_linear_regression.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 15:13:44 2019
+
+@author: Fatemeh
+"""
+
+# Data Preprocessing Template
+
+# Importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values
+#be careful that X is the metrics of feature but y is vector
+y = dataset.iloc[:, 1].values
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
