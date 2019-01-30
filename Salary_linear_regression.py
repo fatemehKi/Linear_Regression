ccd167d3537f59561288e511b81d5b95fb4a9611
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-30 10:17:53 -0500

    Update Salary_linear_regression.py

--- Salary_linear_regression.py
+++ Salary_linear_regression.py
@@ -32,7 +32,7 @@ regressor.fit(X_train, y_train)
 y_pred = regressor.predict(X_test)
 
 
-# Visualising the Training set results
+# Visualising the Training set results ### be careful that the line is coming out of the train sets not the test set
 plt.scatter(X_train, y_train, color = 'red')
 plt.plot(X_train, regressor.predict(X_train), color = 'pink')
 plt.title('Salary vs Experience (Training set)')
