edb03ded734a8f4309078f3f3965460c83d53522
Fatemeh Kiaie <40069260+fatemehKi@users.noreply.github.com>
2019-01-30 08:47:43 -0500

    Update Salary_linear_Regression.R

--- Salary_linear_Regression.R
+++ Salary_linear_Regression.R
@@ -11,6 +11,6 @@ split = sample.split(dataset$Salary, SplitRatio = 2/3)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
-
+#Fitting simple regression into the train set
 regressor = lm(formula = Salary ~ YearsExperience,
                data = training_set)
